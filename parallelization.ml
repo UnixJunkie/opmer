e30ce8756bd78909657f6ef29deef6b5092db428
Francois BERENGER <ligand@free.fr>
2017-09-06 14:35:44 +0900

    moved things in parallelization.ml

---
+++ parallelization.ml
@@ -0,0 +1,23 @@
+
+(* get one job *)
+let get_one to_process () =
+  match !to_process with
+  | [] -> raise Parany.End_of_input
+  | x :: xs ->
+    let res = x in
+    to_process := xs;
+    res
+
+(* perform one job *)
+let process_one fn =
+  Log.debug "%s" fn;
+  (fn, hash_file_persist fn)
+
+let finished = ref 0
+
+(* collect one result *)
+let collect_one nb_files ht (fn, hash) =
+  Ht.add ht fn hash;
+  incr finished;
+  if !finished mod 1000 = 0 then
+    eprintf "done: %.1f%%\r%!" (100.0 *. (float !finished /. float nb_files))
